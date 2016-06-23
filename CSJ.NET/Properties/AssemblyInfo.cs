0cc0fd84f0b057f39566b26855f8727746c81a08
Matt Burke <git@mattburkedev.com>
2016-06-23 18:06:13 -0500

    add nuget files

--- CSJ.NET/Properties/AssemblyInfo.cs
+++ CSJ.NET/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CSJ.NET")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Comma Separated JSON for .NET")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Matt Burke")]
 [assembly: AssemblyProduct("CSJ.NET")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
 [assembly: InternalsVisibleTo("CSJ.NET.Tests")]
