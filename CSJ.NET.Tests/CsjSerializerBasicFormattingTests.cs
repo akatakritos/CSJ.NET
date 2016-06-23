ce8b5e56ba93ebdb9df8d9610e43778b7abe08ad
Matt Burke <git@mattburkedev.com>
2016-06-23 07:45:53 -0500

    formatting of types

---
+++ CSJ.NET.Tests/CsjSerializerBasicFormattingTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using NFluent;
+
+using Xunit;
+
+namespace CSJ.NET.Tests
+{
+    public class CsjSerializerBasicFormattingTests
+    {
+        [Fact]
+        public void Strings_WrappedInDoubleQuotes()
+        {
+            var subject = new CsjSerializer();
+            var strings = new[] { "foo", "bar" };
+
+            var output = subject.Serialize(new List<object[]> { strings });
+
+            Check.That(output).IsEqualTo("\"foo\",\"bar\"" + Environment.NewLine);
+        }
+
+        [Fact]
+        public void Strings_AreEscaped()
+        {
+            var subject = new CsjSerializer();
+            var strings = new[] { "foo\nbar", "quote:\"me\"" };
+
+            var output = subject.Serialize(new List<object[]> { strings });
+
+            Check.That(output).IsEqualTo("\"foo\\nbar\",\"quote:\\\"me\\\"\"" + Environment.NewLine);
+        }
+
+        [Fact]
+        public void Dates_AreISOFormatted()
+        {
+            var subject = new CsjSerializer();
+            var dates = new object[]
+            {
+                new DateTime(2015, 2, 3, 10, 37, 15, DateTimeKind.Utc),
+                new DateTime(2015, 2, 3, 10, 37, 15, DateTimeKind.Local),
+            };
+
+            var output = subject.Serialize(new List<object[]> { dates });
+            var currentOffset = TimeZoneInfo.Local.BaseUtcOffset;
+            var sign = currentOffset.Hours >= 0 ? '+' : '-';
+
+            Check.That(output).IsEqualTo($"\"2015-02-03T10:37:15Z\",\"2015-02-03T10:37:15{sign}{currentOffset:hh':'mm}\"" + Environment.NewLine);
+        }
+
+        [Fact]
+        public void Null_IsALiteral()
+        {
+            var subject = new CsjSerializer();
+            var input = new object[] { null, null };
+
+            var output = subject.Serialize(new List<object[]> { input });
+
+            Check.That(output).IsEqualTo("null,null" + Environment.NewLine);
+        }
+
+        [Fact]
+        public void Numbers_AreLiterals()
+        {
+            var subject = new CsjSerializer();
+            var input = new object[] { 42, 15.759 };
+
+            var output = subject.Serialize(new List<object[]> { input });
+
+            Check.That(output).IsEqualTo("42,15.759" + Environment.NewLine);
+        }
+
+        [Fact]
+        public void NestedArrays()
+        {
+            var subject = new CsjSerializer();
+            var input = new object[] { 42, new[] {"foo","bar"} };
+
+            var output = subject.Serialize(new List<object[]> { input });
+
+            Check.That(output).IsEqualTo("42,[\"foo\",\"bar\"]" + Environment.NewLine);
+        }
+
+        private class NestedObject
+        {
+            public string Foo { get; set; }
+            public string Baz { get; set; }
+        }
+
+        [Fact]
+        public void NestedObjects()
+        {
+            var subject = new CsjSerializer();
+            var input = new object[] { 42, new NestedObject()
+            {
+                Foo = "foo",
+                Baz = "baz"
+            }};
+
+            var output = subject.Serialize(new List<object[]> { input });
+
+            Check.That(output).IsEqualTo("42,{\"Foo\":\"foo\",\"Baz\":\"baz\"}" + Environment.NewLine);
+        }
+    }
+}
---
+++ CSJ.NET/CsjSerializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace CSJ.NET
+{
+    public class CsjSerializer
+    {
+        private string JsonEncode<T>(T o)
+        {
+            return JsonConvert.SerializeObject(o);
+        }
+
+        public string Serialize(IEnumerable<object[]> rows)
+        {
+            var buffer = new StringBuilder();
+            foreach (var row in rows)
+            {
+                var encoded = JsonEncode(row);
+                buffer.AppendLine(encoded.Substring(1, encoded.Length - 2));
+            }
+
+            return buffer.ToString();
+        }
+    }
+}
